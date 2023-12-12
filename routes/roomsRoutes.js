ff1b29bc9ec663e3f4e7f74a054680eac194180d
Atsiilah <atsiilahfailahdall8@gmail.com>
2023-12-12 10:26:01 +0700

    final setup

--- routes/roomsRoutes.js
+++ routes/roomsRoutes.js
@@ -1,9 +1,11 @@
 import { Router } from "express";
-import { getRoomList, postCreateRoom } from "../controllers/roomsControllers.js";
+import { getRoomItem, getRoomList, postCreateRoom } from "../controllers/roomsControllers.js";
 
 const router = Router();
 
+router.get("/", getRoomList);
 router.get("/list", getRoomList);
+router.get("/:id", getRoomItem)
 router.post("/create", postCreateRoom);
 
 export default router;
--- services/roomsServices.js
+++ services/roomsServices.js
@@ -1,9 +1,15 @@
 import Rooms from "../models/rooms.js";
 
+export const getRoomById = async (roomId) => {
+    return await Rooms.findOne({
+        where: { id: roomId },
+    });
+};
+
 export const getAllRooms = async () => {
     return await Rooms.findAll();
 }
 
-export const createRoom = async (name, image,mprice, area, guest, bed, balcony, description) => {
+export const createRoom = async (name, image,price, area, guest, bed, balcony, description) => {
     return await Rooms.create({ name, image, price, area, guest, bed, balcony, description });
 };
